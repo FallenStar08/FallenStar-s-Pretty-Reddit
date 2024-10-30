e2891b3cdb17f04a81980e30d6b8eb7b6674d001
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-10-30 13:24:46 +0100

    cheating

--- js/FallenStarsRedditEnhancements.user.js
+++ js/FallenStarsRedditEnhancements.user.js
@@ -1,18 +1,15 @@
 // ==UserScript==
 // @name        FallenStar's reddit enhancements
 // @namespace   http://tampermonkey.net/
-// @version     1.0.5
+// @version     1.0.6
 // @author      FallenStar
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM.xmlHttpRequest
 // @grant       GM_registerMenuCommand
 // @description Master script to include other userscripts
 // @match       https://old.reddit.com/*
-// @grant       none
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/NavBarFixesModule.user.js
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/CollapsibleSidebarModule.user.js
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/FloatingPanelModule.user.js
-
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
@@ -34,4 +31,28 @@
 		GMStorage.set("panelTop", DEFAULT_PANEL_POSITION.top);
 		alert("All settings reset to defaults.");
 	});
+
+	const scriptUrls = [
+		"https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/NavBarFixesModule.user.js",
+		"https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/CollapsibleSidebarModule.user.js",
+		"https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/FloatingPanelModule.user.js",
+	];
+
+	function loadScript(url) {
+		GM.xmlHttpRequest({
+			method: "GET",
+			url: `${url}?ts=${Date.now()}`, // Prevent caching
+			onload: function (response) {
+				let remoteScript = document.createElement("script");
+				remoteScript.id = "fallen-dev-script"; // Unique ID to identify the script
+				remoteScript.textContent = response.responseText;
+				document.body.appendChild(remoteScript);
+			},
+			onerror: function () {
+				console.error(`Failed to load script: ${url}`);
+			},
+		});
+	}
+
+	scriptUrls.forEach((url) => loadScript(url));
 })();

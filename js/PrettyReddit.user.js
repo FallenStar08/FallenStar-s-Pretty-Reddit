8f3c5eea6ed2c61972ab79060039d2e0997077f5
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2025-08-11 07:32:23 +0200

    fix update link

--- js/PrettyReddit.user.js
+++ js/PrettyReddit.user.js
@@ -9,11 +9,11 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_xmlhttpRequest
 // @run-at      document-end
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/dev/js/pr-navbar.user.js?VERSION
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/dev/js/pr-sidebar.user.js?VERSION
-// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/dev/js/pr-panel.user.js?VERSION
-// @downloadURL https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/dev/js/PrettyReddit.dist.user.js
-// @updateURL 	https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/dev/js/PrettyReddit.dist.user.js
+// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/pr-navbar.user.js?VERSION
+// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/pr-sidebar.user.js?VERSION
+// @require     https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/pr-panel.user.js?VERSION
+// @downloadURL https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/PrettyReddit.dist.user.js
+// @updateURL 	https://github.com/FallenStar08/FallenStar-s-Pretty-Reddit/raw/refs/heads/main/js/PrettyReddit.dist.user.js
 // @author      FallenStar
 // @description Main script for a better reddit
 // ==/UserScript==
